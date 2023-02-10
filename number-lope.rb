a19aa3f95d1cc6a43fce07dc1dc166017688fdbb
yasusu_ 500 <eriyasu@gmail.com>
2023-02-10 15:39:08 +0900

    [Update]分岐条件練習

---
+++ number-lope.rb
@@ -0,0 +1,13 @@
+count = gets.to_i #　標準入力から整数を1個受け取る
+
+count.times do 
+    number = gets.to_i # 標準入力から整数を1個受け取る
+    puts number
+    if number == 0 # もし0であれば
+        puts "#{number}は0"
+    elsif number > 0 # もし0以上であれば
+        puts "#{number}は0より大きい"
+    else # それ以外であれば
+        puts "#{number}は0未満"
+    end
+end
